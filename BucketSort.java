67bb89267ea16eb23cfdccbca9ed9709c4a3fa7a
HeYuyaoo <45375765+HeYuyaoo@users.noreply.github.com>
2018-12-17 19:53:41 +0800

    Update BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -11,16 +11,14 @@ for(int i=1;i<a.length;i++){
   }
 }
 int[]b=new int[temp+1];
-for(int haha=0;haha<b.length;haha++){
-    for(int hehe=0;hehe<a.length;hehe++){
-         if(a[hehe]==haha){
-           b[haha]+=1;
-         }
-    }
-    if(b[haha]!=0){
-      for(int ok=b[haha];ok>0;ok--){
-        system.out.print(b[haha]);
-      }
+int c=0;
+		while(c<b.length) {
+		    for(int ain=0;ain<a.length;ain++){
+		         if(c==a[ain]){
+		           System.out.print(c);
+		         }
+		    }
+		c++;
     }
     
 
